f36cf696d00ddaa1edeabaa58368573a227c79b6
JESUS_DANIEL_FLORES_RODRIGUEZ <a.22308051281186@cbtis128.edu.mx>
2025-03-20 19:02:41 +0000

    P2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Jesús Daniel Flores 1186'),
+          backgroundColor: Color(0xffd52888), // Color de fondo del AppBar
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200, // Propiedad 1: Ancho
+                height: 150, // Propiedad 2: Alto
+                padding:
+                    const EdgeInsets.all(20), // Propiedad 4: Relleno interno
+                decoration: BoxDecoration(
+                  color: Color(0xff26ab78), // Propiedad 5: Color de fondo
+                  borderRadius: BorderRadius.circular(
+                      15), // Propiedad extra: Bordes redondeados
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                      offset: Offset(4, 4), // Propiedad extra: Sombra
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Nombre: Jesús Daniel Flores\nEdad: 17 años\nNo. Control: 1186',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center, // Alinear el texto al centro
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
